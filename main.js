5dc9c494d459b7bbd0beee81761fa14b956a77d4
Meagan Clarke <megclarke@g.ucla.edu>
2025-03-09 16:11:44 -0700

    from 1 to 2.d

--- main.js
+++ main.js
@@ -38,16 +38,31 @@ class Texture_Rotate {
         void main() {    
             // TODO: 2.c Rotate the texture map around the center of each face at a rate of 8 rpm.
 
+            float angle = (4.0 * acos(-1.0) / 15.0) * animation_time; // 4PI/15 rad/sec
+
+            vec2 centered_vUv = vUv - vec2(0.5, 0.5);
+            vec2 new_vUv;
+            new_vUv.x = centered_vUv.x * cos(angle) + centered_vUv.y * sin(angle);
+            new_vUv.y = centered_vUv.x * sin(angle) - centered_vUv.y * cos(angle);
+
+            new_vUv += vec2(0.5, 0.5);
 
             // TODO: 1.b Load the texture color from the texture map
             // Hint: Use texture2D function to get the color of the texture at the current UV coordinates
-            vec4 tex_color = vec4(1.0, 0.0, 0.0, 1.0);
+            vec4 tex_color = texture2D(uTexture, new_vUv);
             
             // TODO: 2.d add the outline of a black square in the center of each texture that moves with the texture
             // Hint: Tell whether the current pixel is within the black square or not using the UV coordinates
             //       If the pixel is within the black square, set the tex_color to vec4(0.0, 0.0, 0.0, 1.0)
+            if ((new_vUv.x > 0.15 && new_vUv.x < 0.85 && 
+                new_vUv.y > 0.15 && new_vUv.y < 0.85) &&
+                !(new_vUv.x > 0.25 && new_vUv.x < 0.75 && 
+                new_vUv.y > 0.25 && new_vUv.y < 0.75)) {
+                tex_color = vec4(0.0, 0.0, 0.0, 1.0);
+            }
 
             gl_FragColor = tex_color;
+
         }
         `;
     }
@@ -76,19 +91,39 @@ class Texture_Scroll_X {
         varying vec3 vPosition;
         void main() {
             // TODO: 2.a Shrink the texuture by 50% so that the texture is repeated twice in each direction
+            vec2 new_vUv = vUv * 2.0;
 
 
             // TODO: 2.b Translate the texture varying the s texture coordinate by 4 texture units per second, 
+            new_vUv.x = new_vUv.x - 4.0 * animation_time;
 
 
             // TODO: 1.b Load the texture color from the texture map
             // Hint: Use texture2D function to get the color of the texture at the current UV coordinates
-            vec4 tex_color = vec4(0.0, 1.0, 0.0, 1.0);
+            vec4 tex_color = texture2D(uTexture, new_vUv);
             
 
             // TODO: 2.d add the outline of a black square in the center of each texture that moves with the texture
             // Hint: Tell whether the current pixel is within the black square or not using the UV coordinates
             //       If the pixel is within the black square, set the tex_color to vec4(0.0, 0.0, 0.0, 1.0)
+
+            float x_min[4] = float[4](0.075, 0.575, 0.075, 0.575);
+            float x_max[4] = float[4](0.425, 0.925, 0.425, 0.925);
+            float y_min[4] = float[4](0.075, 0.075, 0.575, 0.575);
+            float y_max[4] = float[4](0.425, 0.425, 0.925, 0.925);
+ 
+            vec2 vUv_2 = vUv;
+            vUv_2.x = fract(vUv_2.x - 2.0 * animation_time);
+
+            for (int i = 0; i < 4; i += 1) {
+                if ((vUv_2.x > x_min[i] && vUv_2.x < x_max[i] && 
+                    vUv_2.y > y_min[i] && vUv_2.y < y_max[i]) &&
+                    !(vUv_2.x > (x_min[i] + 0.05) && vUv_2.x < (x_max[i] - 0.05) &&
+                    vUv_2.y > (y_min[i] + 0.05) && vUv_2.y < (y_max[i] - 0.05))) {
+                        tex_color = vec4(0.0, 0.0, 0.0, 1.0);
+                }   
+            }
+            
 
             gl_FragColor = tex_color;
         }
@@ -101,11 +136,12 @@ let animation_time = 0.0;
 const cube1_geometry = new THREE.BoxGeometry(2, 2, 2);
 
 // TODO: 1.a Load texture map 
-const cube1_texture = null;
+const cube1_texture = new THREE.TextureLoader().load('assets/stars.png');
 
 // TODO: 1.c Apply Texture Filtering Techniques to Cube 1
 // Nearest Neighbor Texture Filtering
-// e.g. cube1_texture.minFilter = ...
+cube1_texture.minFilter = THREE.NearestFilter;
+cube1_texture.magFilter = THREE.NearestFilter;
 
 
 // TODO: 2.a Enable texture repeat wrapping for Cube 1
@@ -128,14 +164,17 @@ scene.add(cube1_mesh);
 const cube2_geometry = new THREE.BoxGeometry(2, 2, 2);
 
 // TODO: 1.a Load texture map 
-const cube2_texture = null;
+const cube2_texture = new THREE.TextureLoader().load('assets/earth.gif');
 
 // TODO: 1.c Apply Texture Filtering Techniques to Cube 2
 // Linear Mipmapping Texture Filtering
-// e.g. cube2_texture.minFilter = ...
+// e.g. 
+cube2_texture.minFilter = THREE.LinearMipmapLinearFilter;
 
 
 // TODO: 2.a Enable texture repeat wrapping for Cube 2
+cube2_texture.wrapS = THREE.RepeatWrapping;
+cube2_texture.wrapT = THREE.RepeatWrapping;
 
 
 const cube2_uniforms = {
@@ -159,7 +198,8 @@ function animate() {
     controls.update();
 
     // TODO: 2.b&2.c Update uniform values
-    // e.g. cube1_uniforms.animation_time.value = ...
+    cube1_uniforms.animation_time.value = clock.getElapsedTime();
+    cube2_uniforms.animation_time.value = clock.getElapsedTime();
 
 
     // TODO: 2.e Rotate the cubes if the key 'c' is pressed to start the animation
